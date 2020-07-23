546346b27bc463c1505f26f6b391bb9cc36c09ea
adatapost <adatapost@gmail.com>
2020-07-23 10:00:23 +0530

    Add files via upload
    C,C++ and C# code files uploaded

---
+++ Sample.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "\nThis is C++ program";
+  return 0;
+}
---
+++ p1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void help(int topic);
+
+void help(int topic) 
+{
+  printf("A");
+}
+int main()
+{
+  printf("\nHello World");
+  return 0;
+}
